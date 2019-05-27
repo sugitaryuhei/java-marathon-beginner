a83cf0f4e9b1b23e4ec4bc73965b07412271d640
ryuhei.sugita <ryuhei.sugita@rakus.co.jp>
2019-05-27 15:43:37 +0900

    calcControllerの作成

---
+++ src/main/java/com/example/controller/calcController.java
@@ -0,0 +1,34 @@
+package com.example.controller;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/calc")
+public class calcController {
+	
+	@RequestMapping("")
+	public String index() {
+		return "inputvalue";
+	}
+	
+	@Autowired
+	private HttpSession session;
+
+	@RequestMapping("/output")
+	public String output(String num1,String num2) {
+		int answer = Integer.parseInt(num1) * 
+				     Integer.parseInt(num2);
+		session.setAttribute("answer", answer);
+		return "outputvalue";
+	}
+	
+	@RequestMapping("/output2")
+	public String output2() {
+		return "outputvalue2";
+	}
+
+}
