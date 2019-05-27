7942b6e3b6d4629725ead84c6efe9bcd029c3170
ryuhei.sugita <ryuhei.sugita@rakus.co.jp>
2019-05-27 16:23:26 +0900

    userFornの作成

--- src/main/java/com/example/controller/UserInfoController.java
+++ src/main/java/com/example/controller/UserInfoController.java
@@ -1,10 +1,12 @@
 package com.example.controller;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.dmain.User;
+import com.example.form.UserForm;
 
 @Controller
 @RequestMapping("/user-info")
@@ -16,7 +18,9 @@ public class UserInfoController {
 	}
 	
 	@RequestMapping("/output")
-	public String output(User user,Model model) {
+	public String output(UserForm userForm,Model model) {
+		User user = new User();
+		BeanUtils.copyProperties(userForm, user);
 		model.addAttribute("user",user);
 		return "outputuserinfo";
 	}
