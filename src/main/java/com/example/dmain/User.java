75561243811d99f6f6088eeb9335cea0b0f9e242
ryuhei.sugita <ryuhei.sugita@rakus.co.jp>
2019-05-27 17:27:43 +0900

    UserくらすにIdの追加

--- src/main/java/com/example/dmain/User.java
+++ src/main/java/com/example/dmain/User.java
@@ -2,9 +2,17 @@ package com.example.dmain;
 
 public class User {
 	
+	private Integer id;
 	private String name;
 	private Integer age;
 	private String address;
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
 	public String getName() {
 		return name;
 	}
